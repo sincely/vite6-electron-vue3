d46b77f4c9d09f934725a95c6a847977d34d0fe0
chengzhou <1738248438@qq.com>
2026-09-11 16:26:53 +0800

    fix: 移除不必要的 warmup 配置，简化服务器设置

--- vite.config.js
+++ vite.config.js
@@ -51,13 +51,7 @@ export default defineConfig(({ mode, command }) => {
     base: viteEnv.VITE_BASE_URL,
     server: {
       port: 3200, // 指定服务器端口
-      proxy: viteEnv.VITE_USE_PROXY === 'true' ? proxyServer : undefined,
-      warmup: {
-        clientFiles: [
-          './src/render/components/**/*.vue',
-          './src/render/views/**/*.vue'
-        ]
-      }
+      proxy: viteEnv.VITE_USE_PROXY === 'true' ? proxyServer : undefined
     },
     build: {
       // 传递给Terser的更多 minify 选项。
